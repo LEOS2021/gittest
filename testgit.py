7b8af73591ff45c34b2e873acc1b3363d37da939
Ivanov Leonid <leonid@example.com>
2022-06-25 14:16:20 +0300

    hello added

--- testgit.py
+++ testgit.py
@@ -4,6 +4,7 @@ start_time = time.time()
 combo = (9, 9, 7, 6, 5, 4, 3, 2, 1)
 # применить декартово произведение для генерирования перестановок
 # с повтором
+print("Hello")
 for perm in product([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], repeat=len(combo)):
     if perm == combo:
         print("Взломано! {} {}".format(combo, perm))
